65b0f4e7706bd404e6de87609bc6c737a97db4e5
kjasonk <kjasonk@gmail.com>
2017-10-12 22:03:19 -0700

    CRC32
    Added Checksum and CRC32 - Still doesn't work correctly

--- Exercise 2/Ex2Client/src/edu/cpp/cs380/Ex2Client.java
+++ Exercise 2/Ex2Client/src/edu/cpp/cs380/Ex2Client.java
@@ -2,6 +2,7 @@ package edu.cpp.cs380;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.zip.CRC32;
@@ -10,26 +11,34 @@ import java.util.zip.Checksum;
 public class Ex2Client {
 
 	public static void main(String[] args) {
-        byte[] messageByte = new byte[200];
+        byte[] messageByte = new byte[100];
         
 		try (Socket clientSocket = new Socket("18.221.102.182", 38102);
-			)
-			 
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true))
 		{
             DataInputStream in = new DataInputStream(clientSocket.getInputStream());
 
-            for (int x = 0; x < 200; x++){
-                messageByte[x] = in.readByte();
-                System.out.println(messageByte[x]);
-                
+            for (int x = 0; x < 100; x++){
+                messageByte[x] = (byte)((in.readByte() & 0x0f) | (in.readByte()& 0xf0));
+                //System.out.println(messageByte[x]);
             }
             
+            System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(messageByte));
             
+            //CRC32 
             Checksum checksum = new CRC32();
             checksum.update(messageByte, 0, messageByte.length);
             long checksumValue = checksum.getValue();
-            System.out.println("Generated CRC32: " + checksumValue);
-
+            System.out.println("Generated CRC32: " + Long.toHexString(checksumValue));
+            out.println(Long.toHexString(checksumValue));
+            
+            //Checking if CRC is correct
+            byte check = in.readByte();
+            if (check == 1){
+            	System.out.print("Response Good");
+            }
+            else
+            	System.out.println("Response Bad");
 
 		} catch (UnknownHostException e) {
 			System.err.println("Unknown Host");
